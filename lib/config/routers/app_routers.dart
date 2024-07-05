06e93dabbd8ce640dcd98465ecaf9a452be2dde2
josh_MacBook <josuearroliga89@gmail.com>
2024-07-05 17:26:47 -0600

    - Wroked the bottom navigation bar to make it move router. - Added shell route - Implemented current position finder method. - Implemented tap method.

--- lib/config/routers/app_routers.dart
+++ lib/config/routers/app_routers.dart
@@ -1,13 +1,51 @@
 import 'package:cinemapedia/presentation/screens/screens_barrel.dart';
+import 'package:cinemapedia/presentation/views/home_views/favorites_view.dart';
+import 'package:cinemapedia/presentation/views/home_views/home_view.dart';
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 final appRouter = GoRouter(
   initialLocation: '/',
   routes: [
-    GoRoute(
+//Rutas pro medio de shells.
+    ShellRoute(
+      builder: (context, state, child) {
+        return HomeScreen(childView: child);
+      },
+      routes: [
+        //The default path or first screen it will show us.
+        GoRoute(
+            path: '/',
+            builder: (context, state) {
+              return const HomeView();
+            },
+            //Adding the ability to redirect to each movie screen from the home screen.
+            routes: [
+              GoRoute(
+                path: 'movie/:id',
+                name: MovieScreen.name,
+                builder: (context, state) {
+                  final movieId = state.pathParameters['id'] ?? 'no-id';
+                  return MovieScreen(movieId: movieId);
+                },
+              ),
+            ]),
+        GoRoute(
+          path: '/favorites',
+          builder: (context, state) {
+            return const FavoritesView();
+          },
+        ),
+      ],
+    )
+
+    //Rutas padre/hijo
+    /* GoRoute(
         path: '/',
         name: HomeScreen.name,
-        builder: (context, state) => const HomeScreen(),
+        builder: (context, state) => const HomeScreen(
+              childView: FavoritesView(),
+            ),
         routes: [
           GoRoute(
             path: 'movie/:id',
@@ -17,6 +55,6 @@ final appRouter = GoRouter(
               return MovieScreen(movieId: movieId);
             },
           ),
-        ]),
+        ]), */
   ],
 );
--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,118 +1,21 @@
-import 'package:cinemapedia/presentation/providers/movies/initial_loading_provider.dart';
-import 'package:cinemapedia/presentation/providers/movies/movies_providers.dart';
-import 'package:cinemapedia/presentation/providers/movies/slideshow_movies_provider.dart';
-import 'package:cinemapedia/presentation/screens/screens_barrel.dart';
-import 'package:cinemapedia/presentation/widgets/movies/movies_slideshow.dart';
-import 'package:cinemapedia/presentation/widgets/shared/custom_app_bar.dart';
-import 'package:cinemapedia/presentation/widgets/shared/custom_bottom_navigation.dart';
-import 'package:cinemapedia/presentation/widgets/shared/full_screen_loader.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:cinemapedia/presentation/screens/screens_barrel.dart';
 
 class HomeScreen extends StatelessWidget {
   static const name = 'home-screen';
 
-  const HomeScreen({super.key});
+  //Add a final parmeter to select which view we are sending to go router.
+  final Widget childView;
+
+  const HomeScreen({super.key, required this.childView});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: _HomeView(),
-      bottomNavigationBar: CustomBottomNavigation(),
-    );
-  }
-}
-
-//Extracted class to be called
-class _HomeView extends ConsumerStatefulWidget {
-  const _HomeView();
-
-  @override
-  _HomeViewState createState() => _HomeViewState();
-}
-
-class _HomeViewState extends ConsumerState<_HomeView> {
-  @override
-  void initState() {
-    super.initState();
-
-    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
-    ref.read(popularMoviesProvider.notifier).loadNextPage();
-    ref.read(upcomingMoviesProvider.notifier).loadNextPage();
-    ref.read(topRatedMoviesProvider.notifier).loadNextPage();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final initialLoad = ref.watch(initialLoadingProvider);
-
-    if (initialLoad) return const FullScreenLoader();
-
-    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
-    final slideShowMovies = ref.watch(slideShowMoviesProvider);
-    final popularMovies = ref.watch(popularMoviesProvider);
-    final upcomingMovies = ref.watch(upcomingMoviesProvider);
-    final topRatedMovies = ref.watch(topRatedMoviesProvider);
-
-    return CustomScrollView(
-      slivers: [
-        const SliverAppBar(
-          floating: true,
-          flexibleSpace: FlexibleSpaceBar(
-            title: CustomAppBar(),
-          ),
-        ),
-
-//*TODO: A bug was found in this sliver list, the renderflow parent inside flutter was not able to tell the size of each MoviesHorizontalView widgets because the expanded does not offer a constant height, I needed to change the expanded for a container with a fixed height for it to work inside that widget.
-
-        SliverList(
-          delegate: SliverChildBuilderDelegate(
-            (context, index) {
-              return Column(
-                mainAxisSize: MainAxisSize.max,
-                children: [
-                  MoviesSlideshow(movies: slideShowMovies),
-                  MoviesHorizontalListview(
-                    movies: nowPlayingMovies,
-                    title: 'En cines',
-                    subTitle:
-                        '${DateTime.now().day}/${DateTime.now().month}/${DateTime.now().year}',
-                    loadNextPage: () => ref
-                        .read(nowPlayingMoviesProvider.notifier)
-                        .loadNextPage(),
-                  ),
-                  MoviesHorizontalListview(
-                    movies: popularMovies,
-                    title: 'Populares Ahora',
-                    //subTitle: 'Test',
-                    loadNextPage: () =>
-                        ref.read(popularMoviesProvider.notifier).loadNextPage(),
-                  ),
-                  MoviesHorizontalListview(
-                    movies: upcomingMovies,
-                    title: 'A Estrenarse Pronto',
-                    //subTitle: ' Test 3 sub',
-                    loadNextPage: () => ref
-                        .read(upcomingMoviesProvider.notifier)
-                        .loadNextPage(),
-                  ),
-                  MoviesHorizontalListview(
-                    movies: topRatedMovies,
-                    title: 'Mejores Calificadas',
-                    //subTitle: ' Test 3 sub',
-                    loadNextPage: () => ref
-                        .read(topRatedMoviesProvider.notifier)
-                        .loadNextPage(),
-                  ),
-                  const SizedBox(height: 15),
-                ],
-              );
-            },
-            childCount: 1,
-          ),
-        ),
-        //const CustomBottomNavigation(),
-      ],
+    return Scaffold(
+      body: childView,
+      bottomNavigationBar: const CustomBottomNavigation(),
     );
   }
 }
